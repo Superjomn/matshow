5e4a6a703086ce102810fc7aa5d94a5c04c30ece
Superjomn <yanchunwei@outlook.com>
2022-11-13 17:21:22 +0800

    up

--- test/test_draw.py
+++ test/test_draw.py
@@ -9,18 +9,6 @@ def get_test_img_root():
     return os.environ.get("matshow_img_root", "./")
 
 
-def test_Ruler():
-    ruler = draw.Ruler(width=300, height=400, resolution=10)
-    assert ruler.piece == (300 // 10, 400 // 10)
-
-
-def test_recursive_Ruler():
-    ruler0 = draw.Ruler(width=300, height=400, resolution=50)
-    ruler1 = draw.Ruler(width=3, height=4, parent=ruler0)
-    assert ruler0.piece == (300 // 50, 400 / 50)
-    assert ruler1.piece == (ruler0.piece[0] * 3, ruler0.piece[1] * 4)
-
-
 def test_draw_rectangle():
     rec = Rectangle(400,
                     400,
