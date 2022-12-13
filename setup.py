0d25b79cd0dc2963f9113823c5d00d941b781b0d
Superjomn <yanchunwei@outlook.com>
2022-12-13 19:37:21 +0800

    up

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='matshow',
+      version='0.1',
+      description='Matrix visualization toolkit',
+      author='Chunwei Yan',
+      author_email='yanchunwei@outlook.com',
+      # url='',
+      packages=['matshow', 'matshow.widgets'],
+      )
