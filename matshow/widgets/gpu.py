3665a057f6b6fa1ae1efbadbda8acfcde8fa29ca
Superjomn <yanchunwei@outlook.com>
2022-12-13 18:41:21 +0800

    up

--- matshow/widgets/gpu.py
+++ matshow/widgets/gpu.py
@@ -8,13 +8,10 @@ class WarpDataLayout(LabeledWidget):
     '''
     Widget for visualizing the data layout of warp-wize GPU instructions.
     '''
-    random.seed(0)
-    thread_colors = [colors.RGB(*[random.randint(0, 256)
-                                  for i in range(3)]) for i in range(32)]
 
     def __init__(self, shape: List[int], label,
                  thread_to_cell_map: Callable[[int], List[Tuple[int, int]]] = None,
-                 fontsize=40, cell_size: int = 40):
+                 fontsize=40, cell_size: int = 40, random_seed: int = 0):
         '''
         shape: shape of the data.
         label: label of the matrix.
@@ -32,6 +29,10 @@ class WarpDataLayout(LabeledWidget):
         if self.thread_to_cells_map:
             self._colorize_cells()
 
+        random.seed(random_seed)
+        self.thread_colors = [colors.RGB(*[random.randint(0, 256)
+                                           for i in range(3)]) for i in range(32)]
+
     def set_thread_to_cells_map(self, thread_to_cell_map: Callable[[int], List[Tuple[int, int]]]):
         self.thread_to_cells_map = thread_to_cell_map
         self._colorize_cells()
@@ -40,6 +41,6 @@ class WarpDataLayout(LabeledWidget):
         for thread in range(32):
             for (row, col) in self.thread_to_cells_map(thread):
                 cell = self.matrix.get_cell(row, col)
-                cell.fill = WarpDataLayout.thread_colors[thread]
+                cell.fill = self.thread_colors[thread]
                 cell.text("t%d" % thread, fontsize=self.cell_size //
-                          2, fill=colors.YELLOW1)
+                                                   2, fill=colors.YELLOW1)
